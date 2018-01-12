5e9ad1fe094ba659443dce2395a55199c85b8c06
Mauro D'Arcangelo <pmxmd10@gargoyle.maths.nottingham.ac.uk>
2018-01-12 12:56:44 +0000

    developing script

--- script/delta.py
+++ script/delta.py
@@ -33,18 +33,52 @@ def sort_lists(ls1, ls2):
         return 1
 
 
+def contract_list(ls):
+    ls_new = []
+    for item in ls:
+        check_1 = 1
+        for item_new in ls_new:
+            if len(item) == len(item_new):
+                check_2 = 1
+                for idx in range(1, len(item)):
+                    if item[idx] != item_new[idx]:
+                        check_2 = 0
+                if check_2:
+                    item_new[0] = item_new[0] + '+' + item[0]
+                    check_1 = 0
+
+        if check_1:
+            ls_new.append(item)
+
+    for item in ls_new:
+        item[0] = [item[0]]
+
+    return ls_new
+
 n = input('input n: ')
 n = int(n)
 
+
+print('gsl_complex pfac;')
+print('int jfac;')
+
 for s in range(1, n+1):
     terms = []
+    
+    '''
     print('s: ' + str(s))
     print('n-s: ' + str(n-s))
+    '''
 
     # generate i indices
     i = []
+    if n-s:
+        print('int* i' + str(s) + ' = malloc(' + str(n-s) + '*sizeof(int));')
     for num_i in range(n-s):
-        i.append('i[' + str(num_i) + ']')
+        i.append('i' + str(s) + '[' + str(num_i) + ']')
+        print('for(' + i[num_i] + '=0; ' + i[num_i] + '<' + str(n-s) + '; ' + i[num_i] + '++)')
+        print('{')
+
         
     # generate k indices and check that their sum is <= s 
     for num_k in range(int(math.pow(s+1, n-s))):
@@ -159,7 +193,19 @@ for s in range(1, n+1):
                                 if not q[idx_5]:
                                     e.append('e[' + i[idx_5] + ']')
 
+                            if not len(e):
+                                e.append('1')
+
                             #print(e)
+
+                            # 5.1 i factor
+                            temp_51 = []
+                            for idx_51 in range(n-s):
+                                temp_51.append('e[i[' + str(idx_51) + ']]')
+                            if not len(temp_51):
+                                temp_51.append('0')
+                            j = ['0.5*(' + str(n) + '-' + str(s) + '*e[uM]' + '-' + '-'.join(temp_51) + ')']
+
 
 
                             # 6. simplify gammas (gamma[i] squares to e[i]*identity)
@@ -185,19 +231,129 @@ for s in range(1, n+1):
                                 e = [value for value in e if value != item]
                                 if occ % 2:
                                     e.append(item)
-
-
-                            # 8. 
-
-
-
+                                elif not len(e):
+                                    e.append('1')
 
 
                             # add everything to the full lists
-                            terms.append([[binom] + e] + [first] + [second] + [third])
+                            terms.append([str(binom) + '*' + '*'.join(e)] + [j] + [first] + [second] + [third])
+
     
+    # 8. group terms together if they are the same
+    terms = contract_list(terms)
+
+    # 9. dim factors
+    for term in terms:
+        I_count = term.count(['I'])
+        I_g_count = term.count(['I_g'])
+
+        if I_count:
+            term[0][0] = 'dim*(' + term[0][0] + ')'
+            I_count -= 1
+            while I_count:
+                term[0][0] = 'dim*' + term[0][0]
+                I_count -= 1
+
+            while I_g_count:
+                term[0][0] = 'dimG*' + term[0][0]
+                I_g_count -= 1
+        else:
+            if I_g_count:
+                term[0][0] = 'dimG*(' + term[0][0] + ')'
+                I_g_count -= 1
+                while I_g_count:
+                    term[0][0] = 'dimG*' + term[0][0]
+                    I_g_count -= 1
+        
+    terms = [[item for item in term if item != ['I'] and item != ['I_g']] for term in terms]
+
+
+                
+
+
+    # 10. find needed matrix products and traces
+
+    products = []
+
+    for term in terms:
+        for idx_10 in range(2, len(term)):
+            check_10 = 1
+            for product in products:
+                if product == term[idx_10]:
+                    check_10 = 0
+            if check_10:
+                products.append(term[idx_10])
+    
+    ''' 
     for item in terms:
         print(item)
+    print('')
+    for item in products:
+        print(item)
+    '''
+
+    # 11. write code to declare traces
+    for item in products:
+        if len(item) > 1:
+            name = 'array' + str(s) + str(products.index(item))
+            print('gsl_matrix_complex** ' + name + ' = malloc(' + str(len(item)) + '*sizeof(gsl_matrix_complex*));')
+            for idx_11 in range(len(item)):
+                print(name + '[' + str(idx_11) + '] = ' + item[idx_11] + ';')
+
+            res = ''.join(item)
+            if res.find('gamma') != -1:
+                dim = 'dimG'
+            else:
+                dim = 'dim'
+            print('gsl_matrix_complex* ' + res + ' = gsl_matrix_complex_alloc(' + dim + ', ' + dim + ');')
+            print('matrix_multiprod(' + name + ', ' + str(len(item)) + ', ' + res + ');')
+            print('gsl_complex tr' + res + ' = trace(' + res + ');')
+            print('gsl_matrix_complex_free(' + res + ');')
+            print('free(' + name + ');')
+        else:
+            print('gsl_complex tr' + ''.join(item) + ' = trace(' + ''.join(item) + ');')
+
+    
+    # 12. write code to compute action
+    for term in terms:
+        
+        pre = term[0][0]
+        jfac = term[1][0]
+        print('jfac = (int)' + jfac + ' % 4;')
+        print('if(jfac == 0)')
+        print('pfac = gsl_complex_rect(' + pre + ', 0);')
+        print('if(jfac == 1)')
+        print('pfac = gsl_complex_rect(0, ' + pre + ');')
+        print('if(jfac == 2)')
+        print('pfac = gsl_complex_rect(-' + pre + ', 0);')
+        print('if(jfac == 3)')
+        print('pfac = gsl_complex_rect(0, -' + pre + ');')
+        compl = []
+        for idx_12 in range(2, len(term)):
+            compl.append('tr' + ''.join(term[idx_12]))
+        
+        if len(compl) == 3:
+            print('gsl_complex_mul(gsl_complex_mul(gsl_complex_mul(' + compl[0] + ', ' + compl[1] + '), ' + compl[2] + '), pfac);')
+        elif len(compl) == 2:
+            print('gsl_complex_mul(gsl_complex_mul(' + compl[0] + ', ' + compl[1] + '), pfac);')
+        elif len(compl) == 1:
+            print('gsl_complex_mul(' + compl[0] + ', pfac);')
+
+    for val in range(n-s):
+        print('}')
+
+
+
+        
+
+
+
+
+
+
+
+
+
 
 
 
